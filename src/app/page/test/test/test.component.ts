c6437089d66b1d74954f49dd4017fb8961f82eec
AncoIsaac <166188398+AncoIsaac@users.noreply.github.com>
2025-04-15 12:30:37 -0600

    se quita socket-oi

--- src/app/page/test/test/test.component.ts
+++ src/app/page/test/test/test.component.ts
@@ -8,34 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
-export class TestComponent implements OnInit, OnDestroy  {
-  connectionStatus = 'Desconectado';
-
-  constructor(private wsService: WebsocketService) {}
-
-  ngOnInit() {
-    this.wsService.messages$.subscribe({
-      next: (message) => {
-        console.log('Mensaje recibido:', message);
-        // Actualiza tu UI según los mensajes recibidos
-      },
-      error: (err) => console.error('Error:', err)
-    });
-  }
-
-  connect() {
-    this.wsService.connect('e4c92348-ded3-4305-a2d0-506e11218511');
-    this.connectionStatus = 'Conectando...';
-  }
-
-  // sendMove() {
-  //   this.wsService.sendMessage('playerMove', { 
-  //     gameId: 'd72e13eb-e05c-4cff-a325-e1523e35e392',
-  //     move: 'A1' 
-  //   });
-  // }
-
-  ngOnDestroy() {
-    this.wsService.disconnect();
-  }
+export class TestComponent  {
+  
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,64 +1,9 @@
 // websocket.service.ts
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { Manager } from 'socket.io-client'; // Nuevo import
+import { Injectable } from '@angular/core';// Nuevo import
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebsocketService {
-  private socket!: any;
-  private messagesSubject = new Subject<any>();
-  public messages$ = this.messagesSubject.asObservable();
-  private gameId: string = '';
 
-  constructor() {}
-
-  public connect(gameId: string): void {
-    this.gameId = gameId;
-    this.establishConnection();
-  }
-
-  private establishConnection(): void {
-    const manager = new Manager('http://localhost:3000', {
-      reconnectionAttempts: 5,
-      reconnectionDelay: 2000,
-      transports: ['websocket']
-    });
-    const gameId = this.gameId;
-
-    this.socket = manager.socket('/'); // Namespace raíz
-
-    this.socket.on('connect', () => {
-      console.log('this.gameId', this.gameId)
-      console.log('Connected to Socket.io server');
-      this.socket.emit('joinGame', gameId);
-    });
-
-    this.socket.on('message', (data: any) => {
-      this.messagesSubject.next(data);
-    });
-
-    this.socket.on('disconnect', () => {
-      console.log('Disconnected from Socket.io server');
-    });
-
-    this.socket.on('connect_error', (error: any) => {
-      console.error('Connection error:', error);
-    });
-  }
-
-  public sendMessage(event: string, message: any): void {
-    if (this.socket?.connected) {
-      this.socket.emit(event, message);
-    } else {
-      console.warn('Socket not connected. Message not sent:', message);
-    }
-  }
-
-  public disconnect(): void {
-    if (this.socket) {
-      this.socket.disconnect();
-    }
-  }
 }
