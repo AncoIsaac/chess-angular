d83b584caf6a2e971d2532d584ac704489d3dc76
AncoIsaac <166188398+AncoIsaac@users.noreply.github.com>
2025-04-15 15:53:21 -0600

    se agrega websocket

--- src/app/page/test/test/test.component.ts
+++ src/app/page/test/test/test.component.ts
@@ -1,13 +1,45 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { WebsocketService } from '../../../services/websocket.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { WebsocketService } from '../../../services/websocket.service';
 
 @Component({
   selector: 'app-test',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
-export class TestComponent  {
-  
+export class TestComponent implements OnInit, OnDestroy {
+  messages: any[] = [];
+  message = '';
+  private messageSub!: Subscription;
+
+  constructor(private websocketService: WebsocketService) {}
+
+  ngOnInit(): void {
+    this.websocketService.connect();
+    this.messageSub = this.websocketService.getMessage().subscribe(
+      messages => {
+        this.messages.push(messages);
+      }
+    );
+  }
+
+  sendMessage(): void {
+    if (this.message.trim()) {
+      const chatMessage = {
+        content: this.message,
+        sender: 'Usuario' // Puedes reemplazar con el nombre real del usuario
+      };
+      this.websocketService.sendMessage(chatMessage);
+      this.message = '';
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.messageSub.unsubscribe();
+    this.websocketService.disconnect();
+  }
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,9 +1,44 @@
-// websocket.service.ts
-import { Injectable } from '@angular/core';// Nuevo import
+import { Injectable } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class WebsocketService {
+  private client: Client;
+  private messageSubject = new BehaviorSubject<any>(null);
 
+  constructor() {
+    this.client = new Client({
+      brokerURL: 'ws://localhost:8080/ws', // Usa 'wss://' para producción
+      onConnect: () => {
+        this.client.subscribe('/topic/public', message => {
+          this.messageSubject.next(JSON.parse(message.body));
+        });
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+    });
+  }
+
+  connect(): void {
+    this.client.activate();
+  }
+
+  disconnect(): void {
+    this.client.deactivate();
+  }
+
+  sendMessage(message: any): void {
+    this.client.publish({
+      destination: '/app/chat.send',
+      body: JSON.stringify(message)
+    });
+  }
+
+  getMessage(): Observable<any> {
+    return this.messageSubject.asObservable();
+  }
 }
